5c817f0ecedd3d884975d0f7f4a677eda4a286d0
I1HN <malikabertonha@gmail.com>
2026-03-11 10:20:12 -0300

    criado cálculo de dias de oxigênio restantes em Marte

---
+++ script.js
@@ -0,0 +1,10 @@
+let oxigênio = Number(prompt("insira a quantidade de oxigênio restante (em litros):"));
+let pessoas = Number(prompt("insira a quantidade de pessoas em marte:"));
+const consumoDiario = 15; //litros por pessoa.
+
+// consumo total é baseado na quantidade de pessoas multiplicado pelo consumo diario por pessoa.
+let consumoTotalBase = pessoas * consumoDiario;
+let diasRestantes = oxigênio / consumoTotalBase;
+
+alert(`relatório de Marte: Restam: ${diasRestantes.toFixed(0)} dias de oxigênio!!`);
+
